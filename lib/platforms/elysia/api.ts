09ccfb88b6597f28ee74074c6dcd3a4585dc393a
async <router.async@gmail.com>
2026-02-18 13:54:47 +0900

    Refactor API URL handling in Elysia to support different environments

--- lib/platforms/elysia/api.ts
+++ lib/platforms/elysia/api.ts
@@ -5,9 +5,21 @@ import type { TodoApi } from "@/lib/todo-store";
 
 import type { AppType } from "./app";
 
-const url = process.env.NEXT_PUBLIC_VERCEL_URL || 'localhost:3000'
+function getBaseUrl() {
+  if (typeof window !== 'undefined')
+    // browser should use relative path
+    return '';
+  if (process.env.VERCEL_URL)
+    // reference for vercel.com
+    return `https://${process.env.VERCEL_URL}`;
+  if (process.env.RENDER_INTERNAL_HOSTNAME)
+    // reference for render.com
+    return `http://${process.env.RENDER_INTERNAL_HOSTNAME}:${process.env.PORT}`;
+  // assume localhost
+  return `http://localhost:${process.env.PORT ?? 3000}`;
+}
 
-export const { api } = treaty<AppType>(url);
+export const { api } = treaty<AppType>(getBaseUrl());
 
 export const elysiaApi: TodoApi = {
   create: async (body: CreateTodoDto) => {
